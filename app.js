d0b2a99b193c3b41fb42556dc2d87d953d227078
JulienRousset <Julien.rousset33@outlook.fr>
2017-04-07 10:49:23 +0200

    update
    update

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const COMPNT = angular.module('components', []); /// Je crée ma boite composant
 
 angular.module('maboite', []); // je crée ma boite 
 
-const test = angular.module('test', ['components', 'maboite']);  // je déclare test dans mon dossier maboite qui ce trouve dan components
+const test = angular.module('test', ['components', 'maboite']);  // je déclare test dans mon dossier maboite qui ce trouve dans un components
